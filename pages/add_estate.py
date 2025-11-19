914d89cc26b1e60885216ee4b703dcc5f55def20
shad0wrider <71539127+shad0wrider@users.noreply.github.com>
2025-11-19 21:29:16 +0530

    updated add_estate func

--- pages/add_estate.py
+++ pages/add_estate.py
@@ -8,10 +8,10 @@ from streamlit.components.v1 import html
 
 
 st.subheader("Add New Estate")
-name = st.text_input("Estate Name")
-location = st.text_input("Location")
-price = st.number_input("Price (₹)", min_value=0.0, step=1000.0)
-area = st.number_input("Area (sqft)", min_value=0.0)
+name = st.text_input("Estate Name",max_chars=3)
+location = st.text_input("Location",max_chars=3)
+price = st.number_input("Price (₹)", min_value=1.0, step=1000.0)
+area = st.number_input("Area (sqft)", min_value=1.0,step=50.0)
 ptype = st.selectbox("Estate Type", ["Flat", "Villa", "Plot", "Other"])
 
 
@@ -23,19 +23,23 @@ if st.button("Show Location On Map"):
         st.warning("Address of Estate cannot be empty")
     
     else:
-        get_latlong = api.get_location(location)
-        if get_latlong:
-            latitude = get_latlong[0]["lat"]
-            longitude = get_latlong[0]["lon"]
-            place_name = get_latlong[0]["display_name"]
+        try:
+            get_latlong = api.get_location(location)
+            if get_latlong:
+                latitude = get_latlong[0]["lat"]
+                longitude = get_latlong[0]["lon"]
+                place_name = get_latlong[0]["display_name"]
 
-            m = failed.Map(location=[latitude,longitude], zoom_start=23)
-            failed.Marker([longitude,latitude], popup=place_name).add_to(m)
-            html_string = m._repr_html_()  # Get the HTML representation of the map
-            html(html_string, height=500)
+                m = failed.Map(location=[latitude,longitude], zoom_start=23)
+                failed.Marker([longitude,latitude], popup=place_name).add_to(m)
+                html_string = m._repr_html_()  # Get the HTML representation of the map
+                html(html_string, height=500)
 
-        elif get_latlong == "Error" or "Not Found":
-            st.warning("Location not found :(")
+            elif get_latlong == "Error" or "Not Found":
+                st.warning("Location not found :(")
+        except (Exception,ValueError) as iei:
+            st.warning(f"{iei} Happened :(")
+
 
 if st.button("Add Estate"):
     estate.add_estate(name, location, price, area, ptype)
