060481206706fb35f984ad8371647e9dc9be702b
shad0wrider <71539127+shad0wrider@users.noreply.github.com>
2025-11-19 23:54:12 +0530

    - Added home.py - Changed real_estate_appv2.py

---
+++ pages/home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+
+st.markdown("""
+
+## This is a project built to visualize Real Estate Data
+---
+## Libraries Used
+
+- **`streamlit`**  -  **Front-End UI / Webiste**
+- **`sqlite3`**  -  **File-Based Local Database**
+- **`pandas`**  -  **Generating DataFrames and Data Manupilation**
+- **`folium`**  -  **Maps UI**
+- **`requests`**  -  **Maps API**
+            
+`Hosted on GCP and nginx reverse proxy :)`            
+            """)
--- real_estate_appv2.py
+++ real_estate_appv2.py
@@ -60,8 +60,9 @@ st.title("🏠 Real Estate Management System")
 # menus = ["Add Property", "View Properties", "Search Property", "Update Property", "Delete Property"]
 # choice = st.sidebar.radio("Navigation", menus )
 
-page1,page2,page3,page4,page5 = st.columns(5)
+# page1,page2,page3,page4,page5 = st.columns(5)
 
+home = st.Page("pages/home.py",title="About Project")
 
 # ----------- Add Property -----------
 page1 =st.Page("pages/add_estate.py",title="Add New Estate")
@@ -77,6 +78,6 @@ page4 =st.Page("pages/update_estate.py",title="Update Estates")
 # ----------- Delete Property -----------
 page5 =st.Page("pages/delete_estate.py",title="Delete Estate")
 
-mainpage = st.navigation([page1,page2,page3,page4,page5])
+mainpage = st.navigation([home,page1,page2,page3,page4,page5])
 
 mainpage.run()
