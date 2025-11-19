f8dacb78df0c80d91b473d2998fd354457a2e73d
shad0wrider <71539127+shad0wrider@users.noreply.github.com>
2025-11-20 01:24:48 +0530

    - number_input => text_input

--- pages/update_estate.py
+++ pages/update_estate.py
@@ -3,9 +3,11 @@ import streamlit as st
 import real_estate_appv2 as estate
 
 st.subheader("Update Estate Details")
-pid = st.number_input("Enter Estate ID to Update", min_value=1, step=1)
+pid = st.text_input("Enter Estate ID to Update")
 new_price = st.number_input("New Price (₹)", min_value=0.0, step=1000.0)
 new_area = st.number_input("New Area (sqft)", min_value=0.0)
 if st.button("Update"):
-    estate.update_estate(pid, new_price, new_area)
+    if len(pid) == 0:
+        st.error("Estate ID cannot be empty ")
+    estate.update_estate(int(pid), new_price, new_area)
     st.success("✅ Estate updated successfully!")
