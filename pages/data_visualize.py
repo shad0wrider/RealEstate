508e46874e8805626d2a72fea2516b751df8149a
shad0wrider <71539127+shad0wrider@users.noreply.github.com>
2025-11-24 23:02:38 +0530

    - Fixed data_visualize.py

--- pages/data_visualize.py
+++ pages/data_visualize.py
@@ -1,10 +1,37 @@
 import streamlit as st
 import pandas as pd
 import matplotlib as mp
-import numpy as np
+import plotly.express as fancyplot
 
 import real_estate_appv2 as estate
 
-dataframe = pd.DataFrame(estate.get_all_estates())
+data = estate.get_all_estates()
 
-print(dataframe)
+cities = data["location"].unique()
+
+price = data["price"]
+
+feet = data["area_sqft"]
+
+data["avg_price"] = price//feet
+
+
+new = data.groupby("location")["avg_price"].mean()
+
+
+fig = fancyplot.bar(
+    new.reset_index(),x="location",y="avg_price",labels={"location":"City","avg_price":"Price per sqft"},color="location",text="location")
+
+st.plotly_chart(fig)
+
+selector = st.selectbox("Select City",cities)
+
+filtered = data[data["location"]== selector]
+
+counter = filtered["property_type"].value_counts().reset_index()
+
+counter.columns = ["property_type","count"]
+
+fig = fancyplot.pie(counter,names="property_type",values="count")
+
+st.plotly_chart(fig)
